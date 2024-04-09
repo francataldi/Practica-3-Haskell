8064fa2da2e597dfa4e843b7b56a81275a57a964
francataldi <165966507+francataldi@users.noreply.github.com>
2024-04-08 22:00:46 -0300

    Add files via upload

--- test.hs
+++ test.hs
@@ -39,8 +39,12 @@ digitoUnidades x    | x >= 0 = x `mod` 10
                     | otherwise = (x * (-1)) `mod` 10
 
 digitoDecenas :: Int -> Int
-digitoDecenas x | x>= 0 = x `mod` 100 - `mod` 10 div 10
-                | otherwise = (x * (-1)) `mod` 100 - `mod` 10 div 10
+digitoDecenas x     | x >= 0 = x `mod` 100 - x `mod` 10 `div` 10
+                    | otherwise = (x * (-1)) `mod` 100 - x `mod` 10 `div` 10
+
+                    
+
+
 
 
 
