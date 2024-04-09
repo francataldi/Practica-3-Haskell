e969235b50bf1077c6240c3a14ab21664a9e02a2
francataldi <165966507+francataldi@users.noreply.github.com>
2024-04-08 21:29:46 -0300

    Add files via upload

---
+++ test.hs
@@ -0,0 +1,53 @@
+doubleMe x = x + x
+
+----------------
+
+f :: Int -> Int
+f 1 = 8
+f 4 = 131
+f 16 = 16
+
+g :: Int -> Int
+g 8 = 16
+g 16 = 4
+g 131 = 1
+
+--------------------
+
+h :: Int -> Int
+h = f . g
+
+k :: Int -> Int
+k = g . f
+
+--------------------
+
+maximo3 :: Int -> Int -> Int -> Int
+maximo3 x y z   |x >= y && x >= z = x
+                |y >= z = y
+                |otherwise = z
+
+sumaDistintos :: Int -> Int -> Int -> Int
+sumaDistintos x y z |(x /= y) && (x /= z) && (y /= z) = x + y + z
+                    |(x == y) && (x /=z) && (y /= z) = z
+                    |(x == z) && (x /= y) && (y /=z) = y
+                    |(y == z) && (x /=y) && (x/=z) = x
+                    |(x == y) && (x == z) && (y == z) = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
