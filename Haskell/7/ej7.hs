2a278df543e49d7491d57d2726803c35ee3da9e0
francataldi <165966507+francataldi@users.noreply.github.com>
2024-04-10 20:30:37 -0300

    Update ej7.hs

--- Haskell/7/ej7.hs
+++ Haskell/7/ej7.hs
@@ -1,2 +1,2 @@
 distanciaManhattan :: (Float,Float,Float) -> (Float,Float,Float) -> Float
-distanciaManhattan (q,w,e) (a,s,d) = abs ((q - a) + (w - s) + (e - d))
+distanciaManhattan (q,w,e) (a,s,d) = abs(q - a) + abs(w - s) + abs(e - d)
