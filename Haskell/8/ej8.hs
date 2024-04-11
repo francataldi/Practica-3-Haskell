a3e5a27c881826e1d1c0c1a985ee27dac42f846a
francataldi <165966507+francataldi@users.noreply.github.com>
2024-04-10 21:31:28 -0300

    Update ej8.hs

--- Haskell/8/ej8.hs
+++ Haskell/8/ej8.hs
@@ -5,3 +5,11 @@ comparar x y | sumaUltimosDosDigitos y > sumaUltimosDosDigitos x = 1
 
 sumaUltimosDosDigitos :: Int -> Int
 sumaUltimosDosDigitos x = (abs x `mod` 10) + (abs x `div` 10) `mod` 10
+
+--digitoUnidades :: Int -> Int
+--digitoUnidades x = abs x mod 10
+
+--digitoDecenas ::Int -> Int
+--digitoDecenas x = (abs x `div` 10) `mod` 10
+
+--CON ESO ESTOY MODULARIZANDO--
